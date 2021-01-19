094669cddb27f3df08dfcc0d97ff56f16bbb7d55
4a50 <jpjones22@gmail.com>
2021-01-19 08:40:05 -0800

    Added Crocodillia Class

---
+++ Lab6-7/Crocodillia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6_7
+{
+    public abstract class Crocodillia
+    {
+        public string Size { get; set; }
+        public string Snout { get; set; }
+        public string TeethVisible { get; set; }
+        public abstract int averageEggsLaid { get; set; }
+        public bool buildNestOverGround { get; set; }
+
+        public void Attack()
+        {
+            Console.WriteLine("Got you in my teeth now!");
+        }
+
+    }
+}
